92ef72c2ceb3e82ae65955a772c77a4107574d9b
RMhanovernorwichschools <37371520+RMhanovernorwichschools@users.noreply.github.com>
2018-09-07 07:42:15 -0400

    Updated from Brython Server: 9/7/2018 7:42:15 AM

--- distribution.py
+++ distribution.py
@@ -36,8 +36,6 @@ Notice about this example:
   in the text and they are listed in the output in alphabetical order.
 * Letters that do not occur in the text are not listed in the output at all.
 """
-import string
-
 string=input('Please enter a string of text (the bigger the better): ')
 characters=list(string)
 
